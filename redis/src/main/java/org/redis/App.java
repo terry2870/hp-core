30f5576159b2c8125ae0f8b101307df6b34bb235
huangping <terry2870@163.com>
2016-07-17 11:18:01 +0800

    add redis

---
+++ redis/src/main/java/org/redis/App.java
@@ -0,0 +1,13 @@
+package org.redis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
